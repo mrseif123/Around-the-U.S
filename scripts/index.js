3b37774656fb90b494bda23603954786870d1d6c
mrseif123 <codo47@gmail.com>
2021-08-20 13:16:13 +0300

    js work/html fine/css good, form close button needs fixing

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let container = document.querySelectorAll('#body');
+let profile_content = document.querySelector('.profile');
+
+let profile_edit_btn = profile_content.querySelector('.profile__edit-btn');
+let profile_text = profile_content.querySelector('.profile__info');
+
+let profile_name = profile_text.querySelector('.profile__name');
+let profile_subtitle = profile_text.querySelector('.profile__subtitle');
+
+let form_overlay = document.querySelector('.popup-container');
+let form_name_input = form_overlay.querySelector('.form__field_name')
+let form_about_input = form_overlay.querySelector('.form__field_about')
+let form_close_btn = form_overlay.querySelector('.form__close-btn');
+let form_submit_btn = form_overlay.querySelector('.form__submit-btn')
+
+profile_edit_btn.addEventListener("click", function () {
+  form_overlay.style.display = "block";
+  form_name_input.setAttribute("value", profile_name.textContent)
+  form_about_input.setAttribute("value", profile_subtitle.textContent)
+} )
+
+form_close_btn.addEventListener("click", function () {
+  form_overlay.style.display = "none";
+})
+
+form_submit_btn.addEventListener("click", function (event) {
+  event.preventDefault()
+  let name = form_overlay.querySelector('.form__field_name')
+  let about = form_overlay.querySelector('.form__field_about')
+  form_overlay.style.display = "none";
+  console.log(form_name_input.getAttribute("value"))
+  profile_name.textContent = name.value
+  profile_subtitle.textContent = about.value
+})
