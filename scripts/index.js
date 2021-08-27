9b5de4f4b211eafb3f192c96cc9bf61655f1c2e0
mrseif123 <codo47@gmail.com>
2021-08-27 10:49:05 +0300

    JS variables naming

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,39 @@
 let container = document.querySelectorAll('#body');
 
-let profile_content = document.querySelector('.profile');
-let profile_edit_btn = profile_content.querySelector('.profile__edit-btn');
-let profile_text = profile_content.querySelector('.profile__info');
-let profile_name = profile_text.querySelector('.profile__name');
-let profile_subtitle = profile_text.querySelector('.profile__subtitle');
+let profileContent = document.querySelector('.profile');
+let profileEditBtn = profileContent.querySelector('.profile__edit-btn');
+let profileText = profileContent.querySelector('.profile__info');
+let profileName = profileText.querySelector('.profile__name');
+let profileSubtitle = profileText.querySelector('.profile__subtitle');
 
-let form_content = document.querySelector('.popup-container');
-let form_name_input = form_content.querySelector('.form__field-name')
-let form_about_input = form_content.querySelector('.form__field-about')
-let form_close_btn = form_content.querySelector('.form__close-btn');
-let form_submit_btn = form_content.querySelector('.form__submit-btn')
+let formContent = document.querySelector('.popup-container');
+let formNameInput = formContent.querySelector('.form__field-name')
+let formAboutInput = formContent.querySelector('.form__field-about')
+let formCloseBtn = formContent.querySelector('.form__close-btn');
+let formSubmitBtn = formContent.querySelector('.form__submit-btn')
 
-let elements_content = document.querySelector('.elements')
-let heart_icons = elements_content.querySelectorAll('.elements__like-icon')
+let elementsContent = document.querySelector('.elements')
+let heartIcon = elementsContent.querySelectorAll('.elements__like-icon')
 
-profile_edit_btn.addEventListener("click", function () {
-  form_content.style.display = "flex";
-  form_name_input.setAttribute("value", profile_name.textContent)
-  form_about_input.setAttribute("value", profile_subtitle.textContent)
+profileEditBtn.addEventListener("click", function () {
+  formContent.style.display = "flex";
+  formNameInput.setAttribute("value", profileName.textContent)
+  formAboutInput.setAttribute("value", profileSubtitle.textContent)
 } )
 
-form_close_btn.addEventListener("click", function (event) {
+formCloseBtn.addEventListener("click", function (event) {
   event.preventDefault()
-  form_content.style.display = "none";
+  formContent.style.display = "none";
 })
 
-form_submit_btn.addEventListener("click", function (event) {
+formSubmitBtn.addEventListener("click", function (event) {
   event.preventDefault()
-  let name = form_content.querySelector('.form__field-name')
-  let about = form_content.querySelector('.form__field-about')
-  form_content.style.display = "none";
-  console.log(form_name_input.getAttribute("value"))
-  profile_name.textContent = name.value
-  profile_subtitle.textContent = about.value
+  let name = formContent.querySelector('.form__field-name')
+  let about = formContent.querySelector('.form__field-about')
+  formContent.style.display = "none";
+  console.log(formNameInput.getAttribute("value"))
+  profileName.textContent = name.value
+  profileSubtitle.textContent = about.value
 })
 
 
