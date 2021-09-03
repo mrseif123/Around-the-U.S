149b238d610304a7d23367f2feec35616f62df17
mrseif123 <seaf.aliyan@mail.huji.ac.il>
2021-09-03 15:23:23 +0300

    works without  like button

--- scripts/index.js
+++ scripts/index.js
@@ -24,72 +24,122 @@ const initialCards = [{
   }
 ];
 
-let container = document.querySelectorAll('#body');
+const container = document.querySelectorAll('#body');
 
-let profileContent = document.querySelector('.profile');
-let profileEditBtn = profileContent.querySelector('.profile__edit-btn');
-let profileText = profileContent.querySelector('.profile__info');
-let profileName = profileText.querySelector('.profile__name');
-let profileSubtitle = profileText.querySelector('.profile__subtitle');
-let addPlaceBtn = profileContent.querySelector('.profile__add-btn');
+const profileContent = document.querySelector('.profile');
+const profileEditBtn = profileContent.querySelector('.profile__edit-btn');
+const profileText = profileContent.querySelector('.profile__info');
+const profileName = profileText.querySelector('.profile__name');
+const profileSubtitle = profileText.querySelector('.profile__subtitle');
+const addPlaceBtn = profileContent.querySelector('.profile__add-btn');
 
-let popUpContainer = document.querySelector('.popup');
-let addForm = popUpContainer.querySelector(".popup__add-container")
+const popUpContainer = document.querySelector('.popup');
+const addForm = popUpContainer.querySelector(".popup__add-container")
 
-let formNameInput = document.getElementById("fullName")
-let formAboutInput = document.getElementById("about")
+const formNameInput = document.getElementById("fullName")
+const formAboutInput = document.getElementById("about")
 
-let profileForm = popUpContainer.querySelector(".popup__form-container")
-let profileEditCloseBtn = popUpContainer.querySelector('.form__close-btn');
-let editProfileSubmitBtn = document.getElementById("form__profile-submit-button")
+const profileForm = popUpContainer.querySelector(".popup__form-container")
+const profileEditCloseBtn = popUpContainer.querySelector('.form__close-btn');
+const editProfileSubmitBtn = document.getElementById("form__profile-submit-button")
 
-let addElementSubmitBtn = document.getElementById("form__place-submit-button")
-let addPlaceCloseBtn = document.getElementById('close_button_add');
+const addElementSubmitBtn = document.getElementById("form__place-submit-button")
+const addPlaceCloseBtn = document.getElementById('close_button_add');
 
 
-let elementsContent = document.querySelector('.elements')
-let elementsList = elementsContent.querySelector('.elements__list')
-let elementItem = elementsContent.querySelector('.elements__item')
-let likeButton = elementsContent.querySelector('.elements__like-btn')
+const elementsContent = document.querySelector('.elements')
+const elementsList = elementsContent.querySelector('.elements__list')
+const elementItem = elementsContent.querySelector('.elements__item')
+const likeButton = elementsContent.querySelector('.elements__like-btn')
 
-let currentCards = []
+const currentCards = []
 initialCards.forEach(x => addElement(x.name, x.link))
 
+const popups = [addForm, profileForm, popUpContainer]
+
+function hideModel(element){
+  element.classList.add("popup_hidden");
+}
+
+function showModel(element){
+  element.classList.remove("popup_hidden");
+}
+function openModal(element) {
+  element.classList.add("popup_visible");
+}
+
+function closeModal(element) {
+  element.classList.remove("popup_visible");
+}
+
+function openProfilePopup() {
+  openModal(popUpContainer);
+  showModel(popUpContainer);
+
+  openModal(profileForm);
+  showModel(profileForm);
+}
+
+function closeProfilePopup() {
+  closeModal(popUpContainer);
+  closeModal(profileForm);
+  hideAll()
+}
+
+function hideAll(list){
+  list.forEach(x => hideModel(x));
+}
+
 profileEditBtn.addEventListener("click", function () {
-  showPopup(popUpContainer);
-  showPopup(profileForm);
-  let addFormNew = document.getElementById("add_container");
-  addFormNew.style.display = "none"
-  profileForm.style.display = "block"
+  showModel(profileForm)
+  showModel(popUpContainer)
+  hideModel(addForm)
+
+  openProfilePopup()
   formNameInput.value = profileName.textContent
   formAboutInput.value = profileSubtitle.textContent
 })
 
 profileEditCloseBtn.addEventListener("click", function () {
-  hidePopup(popUpContainer);
-  hidePopup(profileForm)
-  hidePopup(addForm)
-  addFormNew.style.display = "none"
-  profileForm.style.display = "none"
-  popUpContainer.style.display = "none"
+  closeProfilePopup()
+  hideAll(popups)
 })
 
+profileForm.addEventListener("submit", function (event) {
+  event.preventDefault()
+  const name = document.getElementById("fullName")
+  const about = document.getElementById("about")
+  profileName.textContent = name.value
+  profileSubtitle.textContent = about.value
+  closeProfilePopup()
+  hideAll(popups)
+
+})
+
+function openAddingPopup() {
+  openModal(popUpContainer);
+  openModal(addForm);
+}
+
+function closeAddingPopup() {
+  closeModal(popUpContainer);
+  closeModal(addForm);
+}
 
 addPlaceBtn.addEventListener("click", function () {
-  showPopup(popUpContainer);
-  showPopup(addForm)
-  let addFormNew = document.getElementById("add_container");
-  profileForm.style.display = "none"
-  addFormNew.style.display = "block"
+  openAddingPopup();
+  showModel(addForm);
+  showModel(popUpContainer);
+  hideModel(profileForm)
 })
 
 addPlaceCloseBtn.addEventListener("click", function () {
-  hidePopup(addForm)
-  hidePopup(popUpContainer)
   const title = document.getElementById("title");
   const link = document.getElementById("link");
   title.value = "";
   link.value = "";
+  closeAddingPopup()
+  hideAll(popups)
 })
 
 addForm.addEventListener("submit", function (event) {
@@ -97,34 +147,24 @@ addForm.addEventListener("submit", function (event) {
   const title = document.getElementById("title");
   const link = document.getElementById("link");
   addElement(title.value, link.value);
-  hidePopup(addForm)
-  hidePopup(popUpContainer)
   title.value = ""
   link.value = ""
-  addFormNew.style.display = "none"
-  profileForm.style.display = "none"
-  popUpContainer.style.display = "none"
+  closeAddingPopup()
+  hideAll(popups)
 })
 
-profileForm.addEventListener("submit", function (event) {
-  event.preventDefault()
-  let name = document.getElementById("fullName")
-  let about = document.getElementById("about")
-  profileName.textContent = name.value
-  profileSubtitle.textContent = about.value
-  hidePopup(profileForm)
-  hidePopup(popUpContainer)
-  addFormNew.style.display = "none"
-  profileForm.style.display = "none"
-  popUpContainer.style.display = "none"
-})
-
-function showPopup(popUpElement) {
-  popUpElement.classList.remove("popup_hidden");
+function openImagePopup(photoElement) {
+  openModal(photoElement);
+  openModal(popUpContainer);
+  hideModel(profileForm)
+  hideModel(addForm)
+  showModel(photoElement)
 }
 
-function hidePopup(popUpElement) {
-  popUpElement.classList.add("popup_hidden");
+function closeImagePopup(photoElement) {
+  closeModal(photoElement);
+  closeModal(popUpContainer);
+  hideAll(popups.concat(photoElement))
 }
 
 function addElement(titleValue, linkValue) {
@@ -132,41 +172,28 @@ function addElement(titleValue, linkValue) {
   const placeElement = elementTemplate.querySelector('.elements__item').cloneNode(true);
   placeElement.querySelector(".elements__title").textContent = titleValue;
   placeElement.querySelector(".elements__img").src = linkValue;
-  
-  placeElement.querySelector(".elements__like-btn").addEventListener("click", function (e) {
-    e.target.classList.toggle("elements__like-btn_active");
-    e.target.classList.toggle("elements__like-btn")
-    console.log(e.target)
+  const likeButton = placeElement.querySelector(".elements__like-btn")
+
+  likeButton.addEventListener("click", function (e) {
+    likeButton.classList.toggle("elements__like-btn_active");
   })
 
   placeElement.querySelector(".elements__delete-btn").addEventListener("click", function (e) {
     placeElement.remove()
   })
-
-  let photoPopupTemplate = document.querySelector("#photo-popup-template").content;
-  let photoElement = photoPopupTemplate.querySelector('.popup__photo').cloneNode(true);
-
+  const photoPopupTemplate = document.querySelector("#photo-popup-template").content;
+  const photoElement = photoPopupTemplate.querySelector('.popup__photo');
   placeElement.querySelector(".elements__img").addEventListener("click", function (e) {
-    titleValue = placeElement.querySelector(".elements__title").textContent;
-    linkValue = placeElement.querySelector(".elements__img").src;
     photoElement.querySelector(".popup__photo-title").textContent = titleValue;
     photoElement.querySelector(".popup__place-image").src = linkValue;
     popUpContainer.insertAdjacentElement("beforeend", photoElement)
-    showPopup(photoElement)
-    showPopup(popUpContainer)
-    addForm.style.display = "none"
-    profileForm.style.display = "none"
-    let closePlacePopup = document.getElementById('close_button_img');
+    openImagePopup(photoElement)
+
+    const closePlacePopup = photoElement.querySelector('.popup__img-close-btn');
     closePlacePopup.addEventListener("click", function () {
-        hidePopup(popUpContainer)
+      closeImagePopup(photoElement)
       photoElement.remove()
     })
   })
   elementsList.prepend(placeElement)
 }
-
-
-// photoPopup.getElementById("close_button_img").addEventListener("click", function () {
-//   hidePopup(popUpContainer)
-//   hidePopup(photoElement)
-// })
