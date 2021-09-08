89056ecce35387f67a04f894c0c3aebea2350981
mrseif123 <seaf.aliyan@mail.huji.ac.il>
2021-09-08 12:11:18 +0300

    proj6 submission 1

--- scripts/index.js
+++ scripts/index.js
@@ -55,9 +55,11 @@ const elementsList = elementsContent.querySelector('.elements__list')
 const elementItem = elementsContent.querySelector('.elements__item')
 const likeButton = elementsContent.querySelector('.elements__like-btn')
 const closePlacePopup = document.querySelector('.popup__img-close-btn');
+const popups = document.querySelectorAll(".popup")
 
-let currentImageElement;
-const currentCards = []
+const containersClasses = ["add-container", "form-container", "photo-container"]
+const popupsObjects = [profileForm, addForm, photoPop]
+
 initialCards.forEach(x => addElement(x.name, x.link))
 
 function openModal(element) {
@@ -71,6 +73,10 @@ function closeModal(element) {
 profileEditBtn.addEventListener("click", function () {
   formNameInput.value = profileName.textContent
   formAboutInput.value = profileSubtitle.textContent
+
+  const inputList = Array.from(profileForm.querySelectorAll(".form__field"));
+  const buttonElement = profileForm.querySelector(".form__submit-btn")
+  toggleButtonState(inputList, buttonElement)
   openProfileForm();
 })
 
@@ -144,24 +150,114 @@ function addElement(titleValue, linkValue) {
 
 function openProfileForm() {
   openModal(profileForm);
+  profileForm.addEventListener("click", mouseHandler);
+  document.addEventListener("keypress", keyHandler);
 }
 
 function closeProfileForm() {
+  profileForm.removeEventListener("click", mouseHandler);
+  document.removeEventListener("keypress", keyHandler);
   closeModal(profileForm);
 }
 
 function openAddFrom() {
   openModal(addForm);
+  addForm.addEventListener("click", mouseHandler);
+  document.addEventListener("keypress", keyHandler);
 }
 
 function closeAddFrom() {
+  addForm.removeEventListener("click", mouseHandler);
+  document.removeEventListener("keypress", keyHandler);
   closeModal(addForm);
 }
 
 function openPhotoPopUp() {
   openModal(photoPop);
+  photoPop.addEventListener("click", mouseHandler);
+  document.addEventListener("keypress", keyHandler);
 }
 
 function closePhotoPopUp() {
+  photoPop.removeEventListener("click", mouseHandler);
+  document.removeEventListener("keypress", keyHandler);
   closeModal(photoPop);
 }
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("form__field_invalid");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("form__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("form__field_invalid");
+  errorElement.classList.remove("form__input-error_active");
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(".form__field"));
+  const buttonElement = formElement.querySelector(".form__submit-btn")
+  toggleButtonState(inputList, buttonElement)
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("form__submit-btn_inactive");
+  } else {
+    buttonElement.classList.remove("form__submit-btn_inactive");
+  }
+};
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll("form"));
+  formList.forEach((formElement) => {
+    formElement.addEventListener("submit", (evt) => {
+      evt.preventDefault;
+    })
+    setEventListeners(formElement);
+  })
+}
+
+function closeOpenedModals(x) {
+  if (x.classList.contains("popup_visible")) {
+    closeModal(x);
+  }
+}
+
+function keyHandler(evt) {
+  if (evt.key === "Escape") {
+    popupsObjects.forEach(popup => closeOpenedModals(popup))
+  }
+}
+
+function mouseHandler(evt) {
+  if (containersClasses.some(r => evt.target.classList.contains(r)))
+    popupsObjects.forEach(popup => closeOpenedModals(popup))
+}
+
+enableValidation();
